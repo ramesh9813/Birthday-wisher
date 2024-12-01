96fd49f9c35a07f801ded2ae38f2f3d7e4b372c5
Ramesh kumar mahato <rameshkumarmahato970@gmail.com>
2024-12-01 15:12:04 +0545

    App is perfectly working

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/4TH_BIRTHDAY_REMINDER/',
+  
   plugins: [react()],
+  base:'/Birthday-wisher',
   server: {
     host: '0.0.0.0',
   }
